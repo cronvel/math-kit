21a0c5c24d28312715fbafbd8fd82e5976f29a85
Cédric Ronvel <cedric.ronvel@gmail.com>
2016-03-23 16:11:11 +0100

    API improvements, Vector3D features

--- test/geometry-test.js
+++ test/geometry-test.js
@@ -29,6 +29,7 @@
 var math = require( '../lib/math.js' ) ;
 var geo = math.geometry ;
 var Vector2D = geo.Vector2D ;
+var Vector3D = geo.Vector3D ;
 var BoundVector2D = geo.BoundVector2D ;
 var expect = require( 'expect.js' ) ;
 
@@ -81,6 +82,7 @@ describe( "Vector2D" , function() {
 	
 	it( "duplicate" , function() {
 		var v1 = Vector2D( 3 , 4 ) ;
+		expect( v1.dup() ).not.to.be( v1 ) ;
 		expect( v1.dup() ).to.eql( { x: 3 , y: 4 } ) ;
 	} ) ;
 	
@@ -101,11 +103,16 @@ describe( "Vector2D" , function() {
 	it( "multiply" , function() {
 		var v1 = Vector2D( 3 , 4 ) ;
 		expect( v1.dup().mul( 3 ) ).to.eql( { x: 9 , y: 12 } ) ;
+		expect( v1.dup().mul( -3 ) ).to.eql( { x: -9 , y: -12 } ) ;
+		expect( v1.dup().mul( 0 ) ).to.eql( { x: 0 , y: 0 } ) ;
 	} ) ;
 	
 	it( "divide" , function() {
 		var v1 = Vector2D( 3 , 4 ) ;
 		expect( v1.dup().div( 4 ) ).to.eql( { x: 0.75 , y: 1 } ) ;
+		expect( v1.dup().div( -4 ) ).to.eql( { x: -0.75 , y: -1 } ) ;
+		expect( v1.dup().div( 0 ) ).to.eql( { x: Infinity , y: Infinity } ) ;
+		//expect( v1.dup().div( 0 ).undefined ).to.be( true ) ;
 	} ) ;
 	
 	it( "inverse" , function() {
@@ -185,19 +192,6 @@ describe( "Vector2D" , function() {
 		expect( Vector2D( 3 , 4 ).cross( Vector2D( -5 , 2 ) ) ).to.be( 26 ) ;
 	} ) ;
 	
-	it( "projections" , function() {
-		var v1 = Vector2D( 3 , 4 ) ;
-		var v2 = Vector2D( 4 , 3 ) ;
-		expect( v1.projectionLength( v2 ) ).to.be( 4.8 ) ;
-		expect( v1.relativeProjectionLength( v2 ) ).to.be( 4.8 ) ;
-		expect( v1.dup().projection( v2 ) ).to.eql( { x: 3.84 , y: 2.88 } ) ;
-		
-		v1.inv() ;
-		expect( v1.projectionLength( v2 ) ).to.be( 4.8 ) ;
-		expect( v1.relativeProjectionLength( v2 ) ).to.be( -4.8 ) ;
-		expect( v1.dup().projection( v2 ) ).to.eql( { x: -3.84 , y: -2.88 } ) ;
-	} ) ;
-	
 	it( "collinear" , function() {
 		expect( Vector2D( 3 , 4 ).isCollinearTo( Vector2D( 4 , 3 ) ) ).to.be( false ) ;
 		expect( Vector2D( 3 , 4 ).isCollinearTo( Vector2D( 3 , 4 ) ) ).to.be( true ) ;
@@ -214,7 +208,18 @@ describe( "Vector2D" , function() {
 		expect( Vector2D( 3 , 4 ).isOrthogonalTo( Vector2D( -4 , 3 ) ) ).to.be( true ) ;
 	} ) ;
 	
-	it( "input errors" ) ;
+	it( "projections" , function() {
+		var v1 = Vector2D( 4 , 3 ) ;
+		var v2 = Vector2D( 3 , 4 ) ;
+		expect( v1.projectionLengthOf( v2 ) ).to.be( 4.8 ) ;
+		expect( v1.projectionValueOf( v2 ) ).to.be( 4.8 ) ;
+		expect( v1.projectionOf( v2 ) ).to.eql( { x: 3.84 , y: 2.88 } ) ;
+		
+		v2.inv() ;
+		expect( v1.projectionLengthOf( v2 ) ).to.be( 4.8 ) ;
+		expect( v1.projectionValueOf( v2 ) ).to.be( -4.8 ) ;
+		expect( v1.projectionOf( v2 ) ).to.eql( { x: -3.84 , y: -2.88 } ) ;
+	} ) ;
 } ) ;
 
 
@@ -222,7 +227,7 @@ describe( "Vector2D" , function() {
 describe( "BoundVector2D" , function() {
 	
 	it( "constructor" , function() {
-		expect( BoundVector2D( { x: 3 , y: 4 } , { x: 5 , y: 2 } ) ).to.eql( { position: { x: 3 , y: 4 } , vector: { x: 5 , y: 2 } } ) ;
+		expect( BoundVector2D( { x: 3 , y: 4 } , { x: 5 , y: 2 } ) ).to.eql( { point: { x: 3 , y: 4 } , vector: { x: 5 , y: 2 } } ) ;
 	} ) ;
 	
 	it( "intersection" , function() {
@@ -233,13 +238,187 @@ describe( "BoundVector2D" , function() {
 		expect( vi ).to.eql( { x: 6 , y: 3 } ) ;
 	} ) ;
 	
-	it( "orthographic projection" , function() {
+	it( "projection (orthographic)" , function() {
 		bv1 = BoundVector2D( { x: 2 , y: 1 } , { x: 2 , y: 1 } ) ;
 		v2 = Vector2D( 3 , 9 ) ;
-		vi = bv1.orthographicProjectionOf( v2 ) ;
+		vi = bv1.projectionOfPoint( v2 ) ;
 		expect( vi ).to.be.an( Vector2D ) ;
 		expect( vi ).to.eql( { x: 6 , y: 3 } ) ;
 	} ) ;
 } ) ;
+
+
+
+describe( "Vector3D" , function() {
+	
+	it( "constructor" , function() {
+		expect( Vector3D( 3 , 4 , 5 ) ).to.eql( { x: 3 , y: 4 , z: 5 } ) ;
+		expect( Number.isNaN( Vector3D().x ) ).to.be( true ) ;
+		expect( Number.isNaN( Vector3D().y ) ).to.be( true ) ;
+		expect( Number.isNaN( Vector3D().z ) ).to.be( true ) ;
+	} ) ;
+	
+	it( "from/to constructor" , function() {
+		expect( Vector3D.fromTo( Vector3D( 3 , 4 , 5 ) , Vector3D( 8 , 2 , -4 ) ) ).to.eql( { x: 5 , y: -2 , z: -9 } ) ;
+	} ) ;
+	
+	it( "undefined vector" , function() {
+		expect( Vector3D().undefined ).to.be( true ) ;
+		expect( Vector3D( NaN , NaN , NaN ).undefined ).to.be( true ) ;
+		expect( Vector3D( undefined , 4 , 5 ).undefined ).to.be( true ) ;
+		expect( Vector3D( 3 , undefined , 5 ).undefined ).to.be( true ) ;
+		expect( Vector3D( 3 , 4 , undefined ).undefined ).to.be( true ) ;
+		expect( Vector3D( 3 , 4 , 5 ).undefined ).to.be( false ) ;
+		
+		var v1 = Vector3D( 3 , 4 , 5 ) ;
+		v1.undefined = true ;
+		expect( Number.isNaN( v1.x ) ).to.be( true ) ;
+		expect( Number.isNaN( v1.y ) ).to.be( true ) ;
+		expect( Number.isNaN( v1.z ) ).to.be( true ) ;
+	} ) ;
+	
+	it( "duplicate" , function() {
+		var v1 = Vector3D( 3 , 4 , 5 ) ;
+		expect( v1.dup() ).not.to.be( v1 ) ;
+		expect( v1.dup() ).to.eql( { x: 3 , y: 4 , z: 5 } ) ;
+	} ) ;
+	
+	it( "addition" , function() {
+		var v1 = Vector3D( 3 , 4 , 5 ) ;
+		var v2 = Vector3D( 5 , 7 , -1 ) ;
+		var v3 = Vector3D( 2 , 1 , 3 ) ;
+		expect( v1.dup().add( v2 ) ).to.eql( { x: 8 , y: 11 , z: 4 } ) ;
+		expect( v1.dup().add( v2 , v3 ) ).to.eql( { x: 10 , y: 12 , z: 7 } ) ;
+	} ) ;
+	
+	it( "substract" , function() {
+		var v1 = Vector3D( 3 , 4 , 5 ) ;
+		var v2 = Vector3D( 2 , 1 , 8 ) ;
+		expect( v1.dup().sub( v2 ) ).to.eql( { x: 1 , y: 3 , z: -3 } ) ;
+	} ) ;
+	
+	it( "multiply" , function() {
+		var v1 = Vector3D( 3 , 4 , 5 ) ;
+		expect( v1.dup().mul( 3 ) ).to.eql( { x: 9 , y: 12 , z: 15 } ) ;
+		expect( v1.dup().mul( -3 ) ).to.eql( { x: -9 , y: -12 , z: -15 } ) ;
+		expect( v1.dup().mul( 0 ) ).to.eql( { x: 0 , y: 0 , z: 0 } ) ;
+	} ) ;
+	
+	it( "divide" , function() {
+		var v1 = Vector3D( 3 , 4 , 5 ) ;
+		expect( v1.dup().div( 4 ) ).to.eql( { x: 0.75 , y: 1 , z: 1.25 } ) ;
+		expect( v1.dup().div( -4 ) ).to.eql( { x: -0.75 , y: -1 , z: -1.25 } ) ;
+		expect( v1.dup().div( 0 ) ).to.eql( { x: Infinity , y: Infinity , z: Infinity } ) ;
+		//expect( v1.dup().div( 0 ).undefined ).to.be( true ) ;
+	} ) ;
+	
+	it( "inverse" , function() {
+		var v1 = Vector3D( 3 , 4 , -5 ) ;
+		expect( v1.dup().inv() ).to.eql( { x: -3 , y: -4 , z: 5 } ) ;
+	} ) ;
+	
+	it( "get/set length" , function() {
+		var v1 = Vector3D( 3 , 4 , 5 ) ;
+		expect( v1.length ).to.be( 7.0710678118654755 ) ;
+		
+		v1.length = 10 ;
+		expect( v1 ).to.eql( { x: 4.242640687119285 , y: 5.65685424949238 , z: 7.071067811865475 } ) ;
+		
+		v1.length = -10 ;
+		expect( v1 ).to.eql( { x: 4.242640687119285 , y: 5.65685424949238 , z: 7.071067811865475 } ) ;
+	} ) ;
+	
+	it( "normalize/unit" , function() {
+		var v1 = Vector3D( 3 , 4 , 5 ) ;
+		v1.normalize() ;
+		expect( v1 ).to.eql( { x: 0.4242640687119285 , y: 0.565685424949238 , z: 0.7071067811865475 } ) ;
+	} ) ;
+	
+	/*
+	it( "get/set/rotate angle" , function() {
+		var v1 = Vector3D( 5 , 10 * Math.sqrt( 3 ) / 2 ) ;
+		expectCirca( v1.angle , Math.PI / 3 ) ;
+		expectCirca( v1.inv().angle , - Math.PI + Math.PI / 3 ) ;
+		
+		v1 = Vector3D( -5 , 10 * Math.sqrt( 3 ) / 2 ) ;
+		expectCirca( v1.angle , 2 * Math.PI / 3 ) ;
+		
+		v1 = Vector3D( 5 , -10 * Math.sqrt( 3 ) / 2 ) ;
+		expectCirca( v1.angle , - Math.PI / 3 ) ;
+		
+		v1 = Vector3D( 5 , 10 * Math.sqrt( 3 ) / 2 ) ;
+		v1.angle = Math.PI / 6 ;
+		expectCirca( v1.x , 10 * Math.sqrt( 3 ) / 2 ) ;
+		expectCirca( v1.y , 5 ) ;
+		
+		v1.rotate( Math.PI / 6 ) ;
+		expectCirca( v1.angle , Math.PI / 3 ) ;
+		expectCirca( v1.x , 5 ) ;
+		expectCirca( v1.y , 10 * Math.sqrt( 3 ) / 2 ) ;
+	} ) ;
+	
+	it( "get/set/rotate angle in degree" , function() {
+		var v1 = Vector3D( 5 , 10 * Math.sqrt( 3 ) / 2 ) ;
+		expectCirca( v1.angleDeg , 60 ) ;
+		expectCirca( v1.inv().angleDeg , -120 ) ;
+		
+		v1 = Vector3D( -5 , 10 * Math.sqrt( 3 ) / 2 ) ;
+		expectCirca( v1.angleDeg , 120 ) ;
+		
+		v1 = Vector3D( 5 , -10 * Math.sqrt( 3 ) / 2 ) ;
+		expectCirca( v1.angleDeg , -60 ) ;
+		
+		v1 = Vector3D( 5 , 10 * Math.sqrt( 3 ) / 2 ) ;
+		v1.angleDeg = 30 ;
+		expectCirca( v1.x , 10 * Math.sqrt( 3 ) / 2 ) ;
+		expectCirca( v1.y , 5 ) ;
+		
+		v1.rotateDeg( 30 ) ;
+		expectCirca( v1.angleDeg , 60 ) ;
+		expectCirca( v1.x , 5 ) ;
+		expectCirca( v1.y , 10 * Math.sqrt( 3 ) / 2 ) ;
+	} ) ;
+	*/
+	
+	it( "dot product" , function() {
+		expect( Vector3D( 3 , 4 , 5 ).dot( Vector3D( 5 , 2 , 1 ) ) ).to.be( 28 ) ;
+		expect( Vector3D( 3 , 4 , 5 ).dot( Vector3D( -5 , 2 , -3 ) ) ).to.be( -22 ) ;
+	} ) ;
+	
+	it( "cross product" , function() {
+		expect( Vector3D( 3 , 4 , 5 ).cross( Vector3D( 5 , 2 , 1 ) ) ).to.eql( { x: -6, y: 22, z: -14 } ) ;
+		expect( Vector3D( 3 , 4 , 5 ).cross( Vector3D( -5 , 2 , -3 ) ) ).to.eql( { x: -22, y: -16, z: 26 } ) ;
+	} ) ;
+	
+	it( "collinear" , function() {
+		expect( Vector3D( 3 , 4 , 5 ).isCollinearTo( Vector3D( 4 , 3 , 5 ) ) ).to.be( false ) ;
+		expect( Vector3D( 3 , 4 , 5 ).isCollinearTo( Vector3D( 3 , 4 , 5 ) ) ).to.be( true ) ;
+		expect( Vector3D( 3 , 4 , 5 ).isCollinearTo( Vector3D( 1.5 , 2 , 2.5 ) ) ).to.be( true ) ;
+		expect( Vector3D( 3 , 4 , 5 ).isCollinearTo( Vector3D( 6 , 8 , 10 ) ) ).to.be( true ) ;
+		expect( Vector3D( 3 , 4 , 5 ).isCollinearTo( Vector3D( -3 , -4 , -5 ) ) ).to.be( true ) ;
+	} ) ;
+	
+	it( "orthogonal" , function() {
+		expect( Vector3D( 3 , 4 , 5 ).isOrthogonalTo( Vector3D( 4 , 3 , 5 ) ) ).to.be( false ) ;
+		expect( Vector3D( 3 , 4 , 5 ).isOrthogonalTo( Vector3D( 3 , 4 , 5 ) ) ).to.be( false ) ;
+		expect( Vector3D( 3 , 4 , 0 ).isOrthogonalTo( Vector3D( 0 , 0 , -1 ) ) ).to.be( true ) ;
+		expect( Vector3D( 3 , 4 , 0 ).isOrthogonalTo( Vector3D( 4 , -3 , 0 ) ) ).to.be( true ) ;
+		expect( Vector3D( 3 , 4 , 2 ).isOrthogonalTo( Vector3D( 4 , -4 , 2 ) ) ).to.be( true ) ;
+	} ) ;
+	
+	it( "projections" , function() {
+		var v1 = Vector3D( 3 , 4 , 5 ) ;
+		var v2 = Vector3D( 4 , 3 , -2 ) ;
+		expect( v1.projectionLengthOf( v2 ) ).to.be( 1.979898987322333 ) ;
+		expect( v1.projectionValueOf( v2 ) ).to.be( 1.979898987322333 ) ;
+		expect( v1.projectionOf( v2 ) ).to.eql( { x: 0.8399999999999999 , y: 1.1199999999999999 , z: 1.4 } ) ;
+		
+		v2.inv() ;
+		expect( v1.projectionLengthOf( v2 ) ).to.be( 1.979898987322333 ) ;
+		expect( v1.projectionValueOf( v2 ) ).to.be( -1.979898987322333 ) ;
+		expect( v1.projectionOf( v2 ) ).to.eql( { x: -0.8399999999999999 , y: -1.1199999999999999 , z: -1.4 } ) ;
+	} ) ;
+} ) ;
+
 
 
